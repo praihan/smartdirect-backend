4c21b8c6597dc8922c8e05b6ff6bed70c5d15f6b
Pranjal Raihan <pranjal@pranjal-raihan.com>
2016-09-13 11:04:21 -0400

    Add User model and authentication

---
+++ app/controllers/health_check_controller.rb
@@ -0,0 +1,9 @@
+class HealthCheckController < ApplicationController
+  before_action :authenticate_user
+
+  def show
+    render json: {
+      status: 'OK'
+    }
+  end
+end
---
+++ test/controllers/health_check_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class HealthCheckControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
