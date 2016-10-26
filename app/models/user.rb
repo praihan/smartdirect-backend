69872811667e92ccefd860fc60ef5dae20319c4f
Pranjal Raihan <pranjal@pranjal-raihan.com>
2016-10-26 17:10:51 -0400

    Stricter checking of getting a user from a token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,22 @@
 class User < ApplicationRecord
   def self.from_token_payload(payload)
-    return self.find_or_create_by identifiable_claim: payload['sub']
+    # This is expected to be in the format
+    # "#{provider}|#{user_id}"
+    sub_claim = payload['sub']
+
+    # Just to be safe...
+    unless sub_claim.is_a? String
+      return nil
+    end
+
+    # Only accept recognized providers, e.g. 'github|', 'google-oauth2|'
+    # Take note of the pipe (|) that separates the provider from their ID
+    known_providers = Settings[:auth0][:known_oauth_providers]
+    if known_providers.none? { |provider| sub_claim.start_with? "#{provider}|" }
+      return nil
+    end
+
+    # Since we provide no sign up mechanism, users are created on the fly
+    return self.find_or_create_by identifiable_claim: sub_claim
   end
 end
