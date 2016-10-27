83c7b87a2691f56390dbed30cf0670d3cbb0c6a1
Pranjal Raihan <pranjal@pranjal-raihan.com>
2016-10-26 23:57:18 -0400

    Fix broken links and incorrect controller name

--- config/initializers/jsonapi-resources.rb
+++ config/initializers/jsonapi-resources.rb
@@ -2,6 +2,11 @@ JSONAPI.configure do |config|
   # Allowed values are :integer(default), :uuid, :string, or a proc
   config.resource_key_type = :uuid
 
+  # Hook up jsonapi-authorization and Pundit
   config.default_processor_klass = JSONAPI::Authorization::AuthorizingProcessor
   config.exception_class_whitelist = [Pundit::NotAuthorizedError]
+
+  # pagination settings
+  config.default_page_size = 10
+  config.maximum_page_size = 30
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'health-check' => 'health_check#index'
 
-  jsonapi_resources :user
+  jsonapi_resources :users
 end
