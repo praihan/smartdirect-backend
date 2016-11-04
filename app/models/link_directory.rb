b250adc5255ec2e04f1dfa67993685c07f220042
Pranjal Raihan <pranjal@pranjal-raihan.com>
2016-11-04 18:26:10 -0400

    Explain use of ltree

--- app/models/link_directory.rb
+++ app/models/link_directory.rb
@@ -4,5 +4,8 @@ class LinkDirectory < ApplicationRecord
   # Deleting is recursive
   has_many :links, dependent: :destroy
 
+  # We can use Postgres' ltree here. Very fast, but limited to alphanumeric
+  # characters. Also, avoids the complexity of nested structures in our code
+  # (PG can do it for us :)
   ltree :path
 end
