460267b9d42262d7e2f2f1b746fc41d6a5ac66a6
Pranjal Raihan <pranjal@pranjal-raihan.com>
2016-11-04 22:09:05 -0400

    Remove temp code

--- app/resources/directory_resource.rb
+++ app/resources/directory_resource.rb
@@ -4,8 +4,4 @@ class DirectoryResource < ApplicationResource
   # A directory only has one User owner at a time
   # See the model, for how this is retrieved
   has_one :user
-
-  def hello
-    'World'
-  end
 end
