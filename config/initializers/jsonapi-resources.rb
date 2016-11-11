07f007e02af656f67657fd4a40f09a8d195a315f
Pranjal Raihan <pranjal@pranjal-raihan.com>
2016-11-11 04:57:03 -0500

    Add commented out config that is not on rubygems yet

--- config/initializers/jsonapi-resources.rb
+++ config/initializers/jsonapi-resources.rb
@@ -2,6 +2,10 @@ JSONAPI.configure do |config|
   # Allowed values are :integer(default), :uuid, :string, or a proc
   # config.resource_key_type = :uuid
 
+  # In debug/test, we all errors to propogate.
+  # This is still in master, unreleased
+  # config.whitelist_all_exceptions = true unless Rails.env.production?
+
   # Hook up jsonapi-authorization and Pundit
   config.default_processor_klass = JSONAPI::Authorization::AuthorizingProcessor
   config.exception_class_whitelist = [Pundit::NotAuthorizedError]
