f3b2cb48d0cc772630dec1d2dc665d8b989ee4b5
Pranjal Raihan <pranjal@pranjal-raihan.com>
2016-11-11 16:22:34 -0500

    Remove unused variables in user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -45,14 +45,14 @@ RSpec.describe User, type: :model do
 
   it 'fails authentication on missing \'email\'' do
     begin
-      user = User.from_token_payload (default_payload.except 'email')
+      User.from_token_payload (default_payload.except 'email')
       fail 'should have failed because \'email\' was not specified'
     rescue Errors::UserError => e
       expect(e.group).to eq('Authentication')
       expect(e.severity).to eq(Errors::CRITICAL)
     end
     begin
-      user = User.from_token_payload (default_payload.merge 'email' => nil)
+      User.from_token_payload (default_payload.merge 'email' => nil)
       fail 'should have failed because \'email\' was nil'
     rescue Errors::UserError => e
       expect(e.group).to eq('Authentication')
@@ -62,14 +62,14 @@ RSpec.describe User, type: :model do
 
   it 'fails authentication on missing \'name\'' do
     begin
-      user = User.from_token_payload (default_payload.except 'name')
+      User.from_token_payload (default_payload.except 'name')
       fail 'should have failed because \'name\' was not specified'
     rescue Errors::UserError => e
       expect(e.group).to eq('Authentication')
       expect(e.severity).to eq(Errors::CRITICAL)
     end
     begin
-      user = User.from_token_payload (default_payload.merge 'name' => nil)
+      User.from_token_payload (default_payload.merge 'name' => nil)
       fail 'should have failed because \'name\' was nil'
     rescue Errors::UserError => e
       expect(e.group).to eq('Authentication')
@@ -79,14 +79,14 @@ RSpec.describe User, type: :model do
 
   it 'fails authentication on missing \'iss\'' do
     begin
-      user = User.from_token_payload (default_payload.except 'iss')
+      User.from_token_payload (default_payload.except 'iss')
       fail 'should have failed because \'iss\' was not specified'
     rescue Errors::UserError => e
       expect(e.group).to eq('Authentication')
       expect(e.severity).to eq(Errors::CRITICAL)
     end
     begin
-      user = User.from_token_payload (default_payload.merge 'iss' => nil)
+      User.from_token_payload (default_payload.merge 'iss' => nil)
       fail 'should have failed because \'iss\' was nil'
     rescue Errors::UserError => e
       expect(e.group).to eq('Authentication')
@@ -96,14 +96,14 @@ RSpec.describe User, type: :model do
 
   it 'fails authentication on missing \'aud\'' do
     begin
-      user = User.from_token_payload (default_payload.except 'aud')
+      User.from_token_payload (default_payload.except 'aud')
       fail 'should have failed because \'aud\' was not specified'
     rescue Errors::UserError => e
       expect(e.group).to eq('Authentication')
       expect(e.severity).to eq(Errors::CRITICAL)
     end
     begin
-      user = User.from_token_payload (default_payload.merge 'aud' => nil)
+      User.from_token_payload (default_payload.merge 'aud' => nil)
       fail 'should have failed because \'aud\' was nil'
     rescue Errors::UserError => e
       expect(e.group).to eq('Authentication')
@@ -113,21 +113,21 @@ RSpec.describe User, type: :model do
 
   it 'fails authentication on invalid \'iat\'' do
     begin
-      user = User.from_token_payload (default_payload.except 'iat')
+      User.from_token_payload (default_payload.except 'iat')
       fail 'should have failed because \'iat\' was not specified'
     rescue Errors::UserError => e
       expect(e.group).to eq('Authentication')
       expect(e.severity).to eq(Errors::CRITICAL)
     end
     begin
-      user = User.from_token_payload (default_payload.merge 'iat' => nil)
+      User.from_token_payload (default_payload.merge 'iat' => nil)
       fail 'should have failed because \'iat\' was nil'
     rescue Errors::UserError => e
       expect(e.group).to eq('Authentication')
       expect(e.severity).to eq(Errors::CRITICAL)
     end
     begin
-      user = User.from_token_payload (default_payload.merge 'iat' => 'hello')
+      User.from_token_payload (default_payload.merge 'iat' => 'hello')
       fail 'should have failed because \'iat\' was not an Integer'
     rescue Errors::UserError => e
       expect(e.group).to eq('Authentication')
@@ -137,21 +137,21 @@ RSpec.describe User, type: :model do
 
   it 'fails authentication on invalid \'exp\'' do
     begin
-      user = User.from_token_payload (default_payload.except 'exp')
+      User.from_token_payload (default_payload.except 'exp')
       fail 'should have failed because \'exp\' was not specified'
     rescue Errors::UserError => e
       expect(e.group).to eq('Authentication')
       expect(e.severity).to eq(Errors::CRITICAL)
     end
     begin
-      user = User.from_token_payload (default_payload.merge 'exp' => nil)
+      User.from_token_payload (default_payload.merge 'exp' => nil)
       fail 'should have failed because \'iat\' was nil'
     rescue Errors::UserError => e
       expect(e.group).to eq('Authentication')
       expect(e.severity).to eq(Errors::CRITICAL)
     end
     begin
-      user = User.from_token_payload (default_payload.merge 'exp' => 'hello')
+      User.from_token_payload (default_payload.merge 'exp' => 'hello')
       fail 'should have failed because \'exp\' was not an Integer'
     rescue Errors::UserError => e
       expect(e.group).to eq('Authentication')
@@ -172,7 +172,7 @@ RSpec.describe User, type: :model do
   it 'fails authentication on unknown claim providers' do
     %w(twitter facebook).each do |provider|
       begin
-        user = User.from_token_payload (default_payload.merge 'sub' => "#{provider}|1234")
+        User.from_token_payload (default_payload.merge 'sub' => "#{provider}|1234")
         fail "should have thrown because of unsupported provider '#{provider}'"
       rescue Errors::UserError => e
         expect(e.group).to eq('Authentication')
