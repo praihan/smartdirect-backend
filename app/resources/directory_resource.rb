439c2bf1d23c219644cce1e27a62d8a6e5ba4533
Pranjal Raihan <pranjal@pranjal-raihan.com>
2016-11-12 15:02:56 -0500

    Correct throw ResourceError if something goes wrong in creating or updating directory

--- app/resources/directory_resource.rb
+++ app/resources/directory_resource.rb
@@ -24,21 +24,24 @@ class DirectoryResource < ApplicationResource
 
 
   # Before we save, make sure we have our relationships properly set up
-  # TODO: create an exception, whitelist it, and use rescue_from
+  # TODO: whitelist exception, and use rescue_from
   before_save do
     parent = @model.parent
-    # This nil checking is critical in making sure that the user is not allowed to
-    # create ROOT directories through the API
-    if parent == nil
-      raise StandardError.new('cannot have a directory without a parent')
-    end
+    action = @model.new_record? ? 'create' : 'update'
     user_link_system_id = context[:user].link_system.id
     # Although this is checked at the ActiveRecord model level, error messages from there
     # are too descriptive. This is because an ID might exist but the user might not be the
     # owner of that directory. Our model will complain about this...but now the user knows
     # that an ID is taken (gasp). So we don't let them know.
-    unless user_link_system_id == parent.link_system_id
-      raise StandardError.new("cannot find parent with id '#{parent.link_system_id}'")
+    # This nil checking is critical in making sure that the user is not allowed to
+    # create ROOT directories through the API
+    if parent == nil || user_link_system_id != parent.link_system_id
+      parent_id = @model.parent_id
+      raise Errors::ResourceError.new(
+          action: "#{action.camelcase} DirectoryResource",
+          message: "Cannot find parent directory with #{parent_id == nil ? 'no id' : "id '#{parent_id}'"}",
+          severity: Errors::MINOR,
+      )
     end
     # Make sure that the user that is creating is the owner. But we only need to do this
     # on create - we don't change this anywhere else
