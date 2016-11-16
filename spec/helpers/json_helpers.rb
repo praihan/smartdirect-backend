e0600fcbf0d27218dbd1aadedc4969653e6fc0f9
Pranjal Raihan <pranjal@pranjal-raihan.com>
2016-11-16 14:54:24 -0500

    Successfully make a test request requiring JWT

--- spec/helpers/json_helpers.rb
+++ spec/helpers/json_helpers.rb
@@ -8,30 +8,28 @@ module RSpecHelpers
       }
     end
 
-    def auth_headers_for(user)
-      token = Knock::AuthToken.new(payload: {
-          'sub' => user.identifiable_claim,
-          'email' => user.email,
-          'name' => user.name,
-
-          # 'iss' => '',
-          # 'aud' => '',
-          # 'exp' => 1234,
-          # 'iat' => 1234,
-      }).token
-      return jwt_auth_headers token
+    def auth_headers_for(user, issued_at: Time.now, expires_in: 10.hours)
+      return auth_headers_from(
+          identifiable_claim: user.identifiable_claim,
+          name: user.name,
+          email: user.email,
+          issued_at: issued_at,
+          expires_in: expires_in
+      )
     end
 
-    def auth_headers_from(identifiable_claim:, name:, email:)
+    def auth_headers_from(identifiable_claim:, name:, email:,
+                          issued_at: Time.now, expires_in: 10.hours)
       token = Knock::AuthToken.new(payload: {
           'sub' => identifiable_claim,
           'email' => email,
           'name' => name,
 
-          # 'iss' => '',
-          # 'aud' => '',
-          # 'exp' => 1234,
-          # 'iat' => 1234,
+          'iss' => Settings[:auth0][:issuer],
+          'aud' => Settings[:auth0][:client_id],
+
+          'exp' => (Time.now + expires_in).to_i,
+          'iat' => issued_at.to_i,
       }).token
       return jwt_auth_headers token
     end
--- spec/requests/directory_resource_spec.rb
+++ spec/requests/directory_resource_spec.rb
@@ -15,4 +15,20 @@ RSpec.describe DirectoryResource, type: :request do
     expect(body).to eq(body_for_invalid_token)
   end
 
+  it 'should work for a newly created user' do
+    headers = default_headers.merge(
+        auth_headers_from(
+            identifiable_claim: 'github|1234',
+            name: 'Donald Duck',
+            email: 'Donald@Duck.com'
+        )
+    )
+    get '/directories', headers: headers
+
+    body = JSON.parse response.body
+
+    # This should just be a success. Not a 201 Created.
+    expect(response.status).to eq(200)
+  end
+
 end
