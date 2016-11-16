6c37f2705fd0850f3b3a532e7ae8b512821d29d3
Pranjal Raihan <pranjal@pranjal-raihan.com>
2016-11-16 17:33:01 -0500

    A paginator for user is kind of dumb

--- app/resources/user_resource.rb
+++ app/resources/user_resource.rb
@@ -3,9 +3,6 @@ class UserResource < ApplicationResource
   # especially since we use OAuth to get most of the info
   immutable
 
-  # Someone who has access to all users might want to look through
-  paginator :offset
-
   # These are the two parts of @model.identifiable_claim
   # See models/user.rb
   attributes :oauth_provider, :oauth_id
