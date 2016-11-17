4da9e7c06b36f85b64d2fc943b17bc99c7eee4ea
Pranjal Raihan <pranjal@pranjal-raihan.com>
2016-11-17 01:03:43 -0500

    Use context for directory resource spec as well

--- spec/requests/directory_resource_spec.rb
+++ spec/requests/directory_resource_spec.rb
@@ -4,31 +4,34 @@ require 'rails_helper'
 
 RSpec.describe DirectoryResource, type: :request do
 
-  it 'should fail without auth token' do
-    get '/directories', headers: default_headers
+  context 'when there is no auth token' do
+    it 'fails with 401-Unauthorized' do
+      get '/directories', headers: default_headers
 
-    body = JSON.parse response.body
+      body = JSON.parse response.body
 
-    # 401 Unauthorized for no token
-    expect(response.status).to eq(401)
-    # The message should not contain any extra info
-    expect(body).to eq(body_for_invalid_token)
+      # 401 Unauthorized for no token
+      expect(response.status).to eq(401)
+      # The message should not contain any extra info
+      expect(body).to eq(body_for_invalid_token)
+    end
   end
 
-  it 'should work for a newly created user' do
-    headers = default_headers.merge(
-        auth_headers_from(
-            identifiable_claim: 'github|1234',
-            name: 'Donald Duck',
-            email: 'Donald@Duck.com'
-        )
-    )
-    get '/directories', headers: headers
+  context 'when first time user' do
+    it 'works and returns 200-Success (not 201-Created)' do
+      headers = default_headers.merge(
+          auth_headers_from(
+              identifiable_claim: 'github|1234',
+              name: 'Donald Duck',
+              email: 'Donald@Duck.com'
+          )
+      )
+      get '/directories', headers: headers
 
-    body = JSON.parse response.body
+      body = JSON.parse response.body
 
-    # This should just be a success. Not a 201 Created.
-    expect(response.status).to eq(200)
+      expect(response.status).to eq(200)
+    end
   end
 
 end
