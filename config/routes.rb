0c229a77c37e420fe8aa48a2654e6533619b7834
Pranjal Raihan <pranjal@pranjal-raihan.com>
2016-11-23 22:06:56 -0500

    Route current_user => current-user

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'health-check' => 'health_check#index'
 
-  get 'current_user' => 'current_user#index'
+  get 'current-user' => 'current_user#index'
 
   jsonapi_resources :users
   jsonapi_resources :directories
