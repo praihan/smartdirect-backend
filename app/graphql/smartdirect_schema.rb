2b3428ca1a09cae310916fba79812bc91640e616
Pranjal Raihan <pranjal@pranjal-raihan.com>
2018-04-06 17:22:29 -0700

    Add mutation.

--- app/graphql/smartdirect_schema.rb
+++ app/graphql/smartdirect_schema.rb
@@ -1,5 +1,6 @@
 SmartdirectSchema = GraphQL::Schema.define do
   instrument(:field, GraphQL::Pundit::Instrumenter.new)
 
-  query(Types::QueryType)
+  query Types::QueryType
+  mutation Types::MutationType
 end
